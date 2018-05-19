c951886ddd88428c836ac4f73ed729e917f86f40
Khilan Ravani <khilanravani@gmail.com>
2018-05-19 16:51:58 +0530

    Some trivial errors

--- src/Graphics/Image/HoughTransform.hs
+++ src/Graphics/Image/HoughTransform.hs
@@ -1,23 +1,26 @@
+{-# LANGUAGE FlexibleContexts #-}
+module Graphics.Image.Processing.HoughTransform where
+
 import Control.Applicative
 import System.Environment (getArgs, getProgName)
 import Control.Monad (forM_, when)
 import Control.Monad.ST
 import qualified Data.Foldable as F (maximum)
-import Data.Massiv.Array.IO
+-- import Data.Massiv.Array.IO
 import Data.List
+import Data.Array.MArray
 
-import Codec.Picture.Types(dropTransparency)
-import Codec.Picture
-import Prelude as P
+import Prelude as P hiding (subtract)
+import Graphics.Image.Processing.Filter	
+import Graphics.Image as GI
 import Graphics.Image.ColorSpace
 import Graphics.Image.IO
 import Graphics.Image.Interface as I 
 import Graphics.Image.Types as IP
 
-
 -- ######### Read Image ##########
-readImageRGB :: Array arr RGB Double => arr -> FilePath -> IO (Image arr RGB Double)
-readImageRGB _ = readImage'
+-- readImageRGB :: Array arr RGB Double => arr -> FilePath -> IO (Image arr RGB Double)
+-- readImageRGB _ = readImage'
 
 -- frog <- readImageRGB VU "images/frog.jpg"
 -- writeImage "images/frog_eye_grid.png" $ pixelGrid 10 $ crop (51, 112) (20, 20) frog
@@ -26,7 +29,6 @@ readImageRGB _ = readImage'
 
 -- ######## Convert to Luma #########
 
-toPixelY :: Pixel cs e -> Pixel Y Double
 toImageY :: (ToY cs e, Array arr cs e, Array arr Y Double) =>
             Image arr cs e
          -> Image arr Y Double
@@ -39,89 +41,92 @@ subtract (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
 dotProduct :: Num x => (x, x) -> (x, x) -> x
 dotProduct (x1, y1) (x2, y2) = (x1 * x2) + (y1 * y2)
  
-magnitude :: Floating x => (x, x) -> x
-magnitude x = sqrt (dotProduct x x)
+mag :: Floating x => (x, x) -> x
+mag x = sqrt (dotProduct x x)
 
+fromIntegralP :: (Integral x, Num y) => (x, x) -> (y, y)
+fromIntegralP (x1, y1) = (fromIntegral x1, fromIntegral y1)
+
+{-
 -- trying something new - Functor usage!
 instance Functor P where
     fmap f (x :| y) = f x :| f y
  
 fromIntegralP :: (Integral a, Num b) => P a -> P b
 fromIntegralP = fmap fromIntegral
+-}
 
 -- ######## Hough-T function begins
 
-hough :: Image PixelRGB8 -> Int -> Int -> Image PixelRGB8
+hough :: Image arr RGBA a -> Int -> Int -> Image arr RGBA a
 hough image thetaSize distSize = hImage
  where
- widthMax = (imageWidth image) - 1
- heightMax = (imageHeight image) - 1
- xCtr = widthMax / 2
- yCtr = heightMax / 2
- lumaImg = toPixelY image
+  widthMax = (GI.rows image) - 1
+  heightMax = (GI.cols image) - 1
+  xCtr = widthMax / 2
+  yCtr = heightMax / 2
+  map = IP.toImageY image
 {- or let arr = arrLightIx2 Par (600 :. 800) {Generated image} // image
       lumaImg = computeAs S $ fmap PixelY arr
 -}
- slope x y =
-     let orig = pixelAt lumaImg x y
-         x_ = pixelAt lumaImg (min (x + 1) widthMax) y
-         y_ = pixelAt lumaImg x (min (y + 1) heightMax)
+  slope x y =
+     let orig = I.read map x y
+         x_ = I.read map (min (x + 1) widthMax) y
+         y_ = I.read map x (min (y + 1) heightMax)
      in fromIntegralP (orig - x_, orig - y_)
-   -- List
-   slopeMap = [ ((x, y), slope x y) | x <- [0 .. widthMax], y <- [0 .. heightMax] ]
+  -- List
+  slopeMap = [ ((x, y), slope (x, y)) | x <- [0 .. widthMax], y <- [0 .. heightMax] ]
   
-   -- Type declaration
-   distMax :: Double
-   distMax = (sqrt . fromIntegral $ height ^ 2 + width ^ 2) / 2
+  -- Type declaration
+  distMax :: Double
+  distMax = (sqrt . fromIntegral $ (heightMax + 1) ^ 2 + (widthMax + 1) ^ 2) / 2
+  
+  minLineLength :: Int
+  minLineLength = 100
    
-   minLineLength :: Int
-   minLineLength = 100
-   
-   maxLineGap :: Int
-   maxLineGap = 10
+  maxLineGap :: Int
+  maxLineGap = 10
 
    
-   accBin = runST $
-     do arr <- new ((0, 0), (thetaSize, distSize)) 0
+  accBin = runST $
+     do arr <- newArray ((0, 0), (thetaSize, distSize)) 0
         forM_ slopeMap $ \((x, y), gradient) -> do
             let (x_, y_) = fromIntegralP ((xCtr, yCtr) `subtract` (x, y))
-            when (magnitude gradient > 127) $
+            when (mag gradient > 127) $
               forM_ [0 .. thetaSize] $ \theta -> do
                 let theta_ =
                       fromIntegral theta * 360 / fromIntegral thetaSize / 180 *
                       pi :: Double
                     distance = cos theta_ * x_ + sin theta_ * y_
-                    distance_ = round  (distance * fromIntegral distSize )/ distMax
+                    fromIntegral distance_ = round  (distance * fromIntegral distSize )/ distMax
                     idx = (theta, distance_)
 				-- optimization possible 
 				-- minLineLength = 100 (pixels) and maxLineGap = 10 (pixels)
                 when (distance_ >= 0 && distance_ < distSize) $
-                  do old <- read arr idx
-                     write arr idx (old + 1)
+                  do old <- readArray arr idx
+                     writeArray arr idx (old + 1)
         return arr
 
-     maxAcc = F.maximum accBin 
-	   -- Generating function
-	   hTransform x y =
+  maxAcc = F.maximum accBin 
+  -- Generating function
+  hTransform x y =
        let l = 255 - round  ((accBin ! (x, y)) / maxAcc * 255)
-       in PixelRGB8 l l l
-     hImage = makeImage hTransform thetaSize distSize
+       in image l l l
+  hImage = makeImage thetaSize distSize hTransform
 
 
 houghIO :: FilePath -> FilePath -> Int -> Int -> IO ()
 houghIO path outpath thetaSize distSize = do
-
-[path, path'] <- getArgs
- eimg <- readImage path
- case eimg of
-   Left err -> putStrLn ("Could not read image: " ++ err)
-   Right (ImageRGB8 image_) -> doImage image_
-   Right (ImageRGBA8 image_) -> doImage $ pixelMap dropTransparency image_
-   _ -> putStrLn "Unexpected Pixel Format"
- where
-   doImage image = do
-     let houghImage = hough image thetaSize distSize
-     writeImage outpath $ ImageRGB8 houghImage
+  eimg <- readImage path
+  case eimg of
+    Left err -> putStrLn ("Could not read image: " ++ err)
+    Right image_ -> doImage image_
+    _ -> putStrLn "Unexpected Pixel Format"
+  where
+    doImage :: Image VS RGBA Double -> IO ()
+    doImage image = do
+      let houghImage = hough image thetaSize distSize
+      writeImage outpath houghImage
 
 {- ######### Helper functions #########
  transpose :: Array arr cs e => Image arr cs e -> Image arr cs e 
@@ -147,16 +152,15 @@ houghIO path outpath thetaSize distSize = do
 
 -}
 
-
+{-
 main :: IO ()
 main = do 
  args <- getArgs
  prog <- getProgName
-  case args of
+ case args of
    [path, outpath, thetaSize, distSize] ->
-     houghIO path outpath (read thetaSize) (read distSize)
+     houghIO path outpath (P.read thetaSize) (P.read distSize)
    _ ->
      putStrLn $
      "Usage: " ++ prog ++ " <image-file> <out-file.png> <width> <height>"
-
-
+-}
