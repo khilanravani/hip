2de8c0e9cf21161e4afe6cb9eb0d77158acd5918
khilanravani <khilanravani@gmail.com>
2018-05-24 15:48:37 +0530

    New Hough1

--- src/Graphics/Image/Processing/Hough.hs
+++ src/Graphics/Image/Processing/Hough.hs
@@ -37,34 +37,56 @@ mag x = sqrt (dotProduct x x)
 fromIntegralP :: (Integral x, Num y) => (x, x) -> (y, y)
 fromIntegralP (x1, y1) = (fromIntegral x1, fromIntegral y1)
 
+minLineLength :: Int
+minLineLength = 100
+   
+maxLineGap :: Int
+maxLineGap = 10
+
 hough
   :: forall arr a.
-     (Array arr RGB a, Elevator a, Fractional a)
+     (Array arr RGB a, Elevator a, Fractional a, Array arr Y Double, MArray arr Y Double)
   => Image arr RGB a
   -> Image arr RGB a
 hough image = hImage
  where
    widthMax = ((rows image) - 1) 
-   xCtr = (widthMax / 2)
+   xCtr = (widthMax `div` 2)
    heightMax = ((cols image) - 1)
-   yCtr = (heightMax / 2)
+   yCtr = (heightMax `div` 2)
    luma = IP.toImageY image  
-   
    slope x y =
      let orig = I.index luma (xCtr, yCtr) 
          x_ = I.index luma (widthMax, y)		 
          y_ = I.index luma (x, heightMax)
      in (orig - x_, orig - y_)
+   slopeMap = [ ((x, y), slope x y) | x <- [0 .. widthMax], y <- [0 .. heightMax] ]
+   distMax :: Double
+   distMax = (sqrt . fromIntegral $ (heightMax + 1) ^ 2 + (widthMax + 1) ^ 2) / 2
   
-   gradMap =
-     [ ((x, y), slope x y)
-     | x <- [0 .. widthMax]
-     , y <- [0 .. heightMax] ]
-   
+   accBin = runST $
+     do arr <- new (widthMax, heightMax)
+        forM_ slopeMap $ \((x, y), gradient) -> do
+            let (x_, y_) = fromIntegralP ((xCtr, yCtr) `sub` (x, y))
+            when ((mag gradient) > 127) $
+              forM_ [0 .. widthMax] $ \theta -> do
+                let theta_ =
+                      fromIntegral theta * 360 / fromIntegral heightMax / 180 *
+                      pi :: Double
+                    distance = round (cos theta_ * x_ + sin theta_ * y_) * ( widthMax / fromIntegral distMax)
+                    idx = (theta, distance)
+                when (distance>= 0 && distance < heightMax) $
+                  do old <- I.read arr idx
+                     write arr idx (old + 1)
+        return arr
 
-
-   hImage :: Image arr RGB a
-   hImage = makeImage (200, 200) (\(i, j) -> PixelRGB (fromIntegral i) (fromIntegral j) (fromIntegral (i + j)) / 400)
+   maxAcc = F.maximum accBin 
+   hTransform x y =
+       let l = 255 - round ((I.index accBin (x, y)) /255 ) * maxAcc
+       in PixelRGB l l l
+   hImage :: Image arr RGB Integer
+   hImage = makeImage (widthMax, heightMax) hTransform
+   --hImage = makeImage (200, 200) (\(i, j) -> PixelRGB (fromIntegral i) (fromIntegral j) (fromIntegral (i + j)) / 400)
 
 main :: IO()
 main = do
