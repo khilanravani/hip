1c7913f1c07fe6a4f2c28a19ada4eeb169ee962e
khilanravani <khilanravani@gmail.com>
2018-05-25 13:34:48 +0530

    Improvised Hough

--- src/Graphics/Image/Processing/Hough.hs
+++ src/Graphics/Image/Processing/Hough.hs
@@ -9,7 +9,6 @@ import Control.Monad (forM_, when)
 import Control.Monad.ST
 import qualified Data.Foldable as F (maximum)
 import Data.List
---import Data.Array.MArray
 
 import Prelude as P hiding (subtract)
 import Graphics.Image.Processing.Filter	
@@ -19,12 +18,15 @@ import Graphics.Image.IO
 import Graphics.Image.Interface as I 
 import Graphics.Image.Types as IP
 
+-- ####### Some trivial functions ########
 toImageY :: (ToY cs e, Array arr cs e, Array arr Y Double) =>
             Image arr cs e
          -> Image arr Y Double
 toImageY = I.map toPixelY
 
--- ####### Some trivial functions ########
+minLineLength :: Int
+minLineLength = 100
+
 sub :: Num x => (x, x) -> (x, x) -> (x, x)
 sub (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
 
@@ -36,9 +38,6 @@ mag x = sqrt (dotProduct x x)
 
 fromIntegralP :: (Integral x, Num y) => (x, x) -> (y, y)
 fromIntegralP (x1, y1) = (fromIntegral x1, fromIntegral y1)
-
-minLineLength :: Int
-minLineLength = 100
    
 maxLineGap :: Int
 maxLineGap = 10
@@ -47,10 +46,8 @@ hough
   :: forall arr a b.
      ( Array arr RGB a, Array arr RGB b
      , Array arr Y Int, MArray arr Y Int
-     , Array arr Y Double, MArray arr Y Double
-     )
-  => Image arr RGB a
-  -> Image arr RGB b
+     , Array arr Y Double, MArray arr Y Double) => Image arr RGB a -> Image arr RGB b
+
 hough image = hImage
  where
    widthMax, xCtr, heightMax, yCtr :: Int
@@ -64,10 +61,10 @@ hough image = hImage
 
    slope :: Int -> Int -> (Double, Double)
    slope x y =
-     let PixelY orig = I.index luma (xCtr, yCtr)
-         PixelY x_ = I.index luma (widthMax, y)
-         PixelY y_ = I.index luma (x, heightMax)
-     in (orig - x_, orig - y_)
+     let PixelY orig = I.index luma (x, y)
+         PixelY x' = I.index luma (min (x+1) widthMax, y)
+         PixelY y' = I.index luma (x, min (y+1) heightMax)
+     in (orig - x', orig - y')
    slopeMap :: [ ((Int, Int), (Double, Double)) ]
    slopeMap = [ ((x, y), slope x y) | x <- [0 .. widthMax], y <- [0 .. heightMax] ]
 
@@ -78,13 +75,13 @@ hough image = hImage
    accBin = runST $
      do arr <- new (widthMax, heightMax)
         forM_ slopeMap $ \((x, y), gradient) -> do
-            let (x_, y_) = fromIntegralP ((xCtr, yCtr) `sub` (x, y))
+            let (x', y') = fromIntegralP ((xCtr, yCtr) `sub` (x, y))
             when ((mag gradient) > 127) $
               forM_ [0 .. widthMax] $ \theta -> do
                 let theta_ =
                       fromIntegral theta * 360 / fromIntegral heightMax / 180 *
                       pi :: Double
-                    distance = cos theta_ * x_ + sin theta_ * y_ * ( fromIntegral widthMax / distMax)
+                    distance = cos theta_ * x' + sin theta_ * y' * ( fromIntegral widthMax / distMax)
                     idx = (theta, round distance)
                 when (distance>= 0 && distance < fromIntegral heightMax) $
                   do old <- I.read arr idx
@@ -92,10 +89,7 @@ hough image = hImage
         freeze arr
 
    maxAcc :: Int
-   PixelY maxAcc
-     = I.fold (\p1@(PixelY a) p2@(PixelY b) -> if a < b then p2 else p1)
-              (PixelY 0)
-              accBin
+   PixelY maxAcc = I.fold (\p1@(PixelY a) p2@(PixelY b) -> if a < b then p2 else p1) (PixelY 0) accBin
 
    hTransform :: (Int, Int) -> Pixel RGB b
    hTransform (x, y) =
@@ -104,12 +98,11 @@ hough image = hImage
        in PixelRGB l l l
 
    hImage = makeImage (widthMax, heightMax) hTransform
-   --hImage = makeImage (200, 200) (\(i, j) -> PixelRGB (fromIntegral i) (fromIntegral j) (fromIntegral (i + j)) / 400)
-
-{- run 'test' in ghci -}
+   
 test :: IO ()
 test = do
   let frog :: Image VU RGB Double
       frog = makeImageR VU (200, 200) (\(i, j) -> PixelRGB (fromIntegral i) (fromIntegral j) (fromIntegral (i + j)) / 400) 
-  writeImage "test.png" frog 
-  --hough frog
+  let houghImage :: Image VU RGB Double
+      houghImage = hough frog
+  writeImage "test.png" houghImage
