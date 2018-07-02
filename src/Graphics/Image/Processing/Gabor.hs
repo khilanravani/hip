e95cf044b5f5a765e9410633025619889df68ad2
khilanravani <khilanravani@gmail.com>
2018-07-02 16:25:38 +0530

    Few corrections

--- src/Graphics/Image/Processing/Gabor.hs
+++ src/Graphics/Image/Processing/Gabor.hs
@@ -47,23 +47,23 @@ gaborFilter dir !border =
          forM_ [0 .. u] $ \i -> do
            forM_ [0 .. v] $ \j -> do
              do arr <- newArray (m, n) 
-             forM_ [0 .. m] $ \x -> do
-               forM_ [0 .. n] $ \y -> do
-                 let px = gaborfn (x, y, σ, θ, γ, λ, ψ)
-                 writeArray arr (x, y) px
-             let idx = (i, j)
-             writeArray gArray idx arr
+                forM_ [0 .. m] $ \x -> do
+                  forM_ [0 .. n] $ \y -> do
+                    let px = gaborfn (x, y, σ, θ, γ, λ, ψ)
+                    writeArray arr (x, y) px
+                let idx = (i, j)
+                writeArray gArray idx arr
+         freeze gArray
 
-
-gaborfeatures
+gaborResult
   :: forall arr e cs . ( MArray arr Y Double, IP.Array arr Y Double, IP.Array arr Y Word16, MArray arr Y Word16, Array arr X Double)
   => Image arr Y Double
-  -> Array arr Y Double
+  -> 
   -> Int  
   -> Int   
   -> Image arr Y Word16
 
-gaborfeatures image gArray thetaSz distSz = I.map (fmap toWord16) accBin
+gaborResult image gArray thetaSz distSz = I.map (fmap toWord16) accBin
  where
    u = 5
    v = 8
@@ -74,19 +74,4 @@ gaborfeatures image gArray thetaSz distSz = I.map (fmap toWord16) accBin
           forM_ [0 .. v] $ \j -> do
             let ip = applyFilter (gaborFilter I.index gArray (i,j)) image 
             writeArray gResult (i, j) ip  
-   
-   features :: Image arr Y Word16
-   features = runST $            
-     do featureVector <- I.new (u, v)   
-        forM_ [0 .. u] $ \i -> do
-          forM_ [0 .. v] $ \j -> do
-            let gaborAbs = abs (I.index gResult (i, j))
-                gaborAbsDs = downsample ((0 ==) . (`mod` 4)) gaborAbs
-            let gaborAbsTr = transpose (gaborAbs)
-                gaborAbsTrDs = downsample ((0 ==) . (`mod` 4)) gaborAbs
-            let gaborFinal = (gaborAbsDs - mean(gaborAbsDs))/ stddev (gaborAbsDs)
-            I.write featureVector (i, j) (PixelY (fromIntegral gaborFinal))
-     
-        freeze featureVector
-
-
+ 
